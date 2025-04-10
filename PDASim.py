092d63094be18948d50ba0b3a6cba2404369187c
Chase_Fetherling <cmfetherling@crimson.ua.edu>
2025-04-10 18:53:55 -0500

    Added pseudostack logic

--- PDASim.py
+++ PDASim.py
@@ -3,7 +3,6 @@ currentState = q0
 stack = []
 for inputChar in input: 
     for transition in transitions:
-        if transition.state == currentState and transition.character == inputChar:             #if transition exists for the state we are in
+        if transition.state == currentState and transition.character == inputChar and (transition.stack == stack[stackhead] or transition.stack == "epsilon"):             #if transition exists for the state we are in
             currentState = transition.nextState            #Do transition
             stack.push(transition.stack)
-    
