9748a246e3e06e01a4fc5580201044bbe17a8240
Robert Powell <repowell1@crimson.ua.edu>
2025-04-10 22:09:46 -0500

    movin thingamajig

--- GUI.py
+++ GUI.py
@@ -22,14 +22,29 @@ def on_drag(event, states, matrix):
 def draw_arrow(matrix, states, index1, index2):
     state1 = states[index1]
     state2 = states[index2]
-    # Get central coords of each
+    # Get coordinates of each circle
     state1_coords = canvas.coords(state1)
-    state1_center = ((state1_coords[0] + state1_coords[2]) / 2, (state1_coords[1] + state1_coords[3]) / 2)
     state2_coords = canvas.coords(state2)
+
+    # Calculate the center of each circle
+    state1_center = ((state1_coords[0] + state1_coords[2]) / 2, (state1_coords[1] + state1_coords[3]) / 2)
     state2_center = ((state2_coords[0] + state2_coords[2]) / 2, (state2_coords[1] + state2_coords[3]) / 2)
-    newArrow = canvas.create_line(state1_center[0], state1_center[1], state2_center[0], state2_center[1], arrow=tk.LAST)
-    matrix[index1][index2] = newArrow
-    # return newArrow
+
+    # Calculate the vector from state1 to state2
+    dx = state2_center[0] - state1_center[0]
+    dy = state2_center[1] - state1_center[1]
+    distance = (dx**2 + dy**2)**0.5
+
+    # Calculate the points on the edge of each circle
+    state1_edge = (state1_center[0] + dx / distance * circle_radius, state1_center[1] + dy / distance * circle_radius)
+    state2_edge = (state2_center[0] - dx / distance * circle_radius, state2_center[1] - dy / distance * circle_radius)
+
+    # Draw the arrow
+    newArrow = canvas.create_line(state1_edge[0], state1_edge[1], state2_edge[0], state2_edge[1], arrow=tk.LAST)
+    if matrix[index1][index2] != 0:
+        matrix[index1][index2].append(newArrow)
+    else:
+        matrix[index1][index2] = [newArrow]
 
 #update all arrows associated with the selected state
 def update_arrows(states, matrix, selectedState):
@@ -40,30 +55,41 @@ def update_arrows(states, matrix, selectedState):
             break
     # update outward arrows
     destinationID = 0
-    for arrowToDestination in matrix[sourceID]:
-        if arrowToDestination != 0:
-            update_arrow(arrowToDestination, states, sourceID, destinationID)
+    for arrowsToDestination in matrix[sourceID]:
+        if arrowsToDestination != 0:
+            for arrow in arrowsToDestination:
+                update_arrow(arrow, states, sourceID, destinationID)
         destinationID += 1
     # now update inward arrows
     destinationID = sourceID
     sourceID = 0
-    for arrowsFromSource in matrix:
-        if arrowsFromSource[destinationID] != 0:
-            update_arrow(arrowsFromSource[destinationID], states, sourceID, destinationID)
+    for arrowListsFromSource in matrix:
+        if arrowListsFromSource[destinationID] != 0:
+            for arrow in arrowListsFromSource[destinationID]:
+                update_arrow(arrow, states, sourceID, destinationID)
         sourceID += 1
     
 
 def update_arrow(arrow, states, id1, id2):
-    # Get the center of the circle
+    # Get the center of the first circle
     state1_coords = canvas.coords(states[id1])
     state1_center = ((state1_coords[0] + state1_coords[2]) / 2, (state1_coords[1] + state1_coords[3]) / 2)
 
-    # Get the center of the rectangle
+    # Get the center of the second circle
     state2_coords = canvas.coords(states[id2])
     state2_center = ((state2_coords[0] + state2_coords[2]) / 2, (state2_coords[1] + state2_coords[3]) / 2)
 
-    # Update the arrow to connect the two centers
-    canvas.coords(arrow, state1_center[0], state1_center[1], state2_center[0], state2_center[1])
+    # Calculate the vector from state1 to state2
+    dx = state2_center[0] - state1_center[0]
+    dy = state2_center[1] - state1_center[1]
+    distance = (dx**2 + dy**2)**0.5
+
+    # Calculate the points on the edge of each circle
+    state1_edge = (state1_center[0] + dx / distance * circle_radius, state1_center[1] + dy / distance * circle_radius)
+    state2_edge = (state2_center[0] - dx / distance * circle_radius, state2_center[1] - dy / distance * circle_radius)
+
+    # Update the arrow to connect the edges of the circles
+    canvas.coords(arrow, state1_edge[0], state1_edge[1], state2_edge[0], state2_edge[1])
 
 
 # Create the main application window
@@ -85,7 +111,7 @@ for i in range(n):
     y1 = circle_spacing
     x2 = x1 + circle_radius * 2
     y2 = y1 + circle_radius * 2
-    circle = canvas.create_oval(x1, y1, x2, y2, fill="blue", tags=["draggable", f"state_{i}"])
+    circle = canvas.create_oval(x1, y1, x2, y2, fill="blue", tags=["draggable", f"state_{i}", "final"])
     states.append(circle)
 
 # Initialized to 0 -- if there is no arrow, will be 0. If there is an arrow, it is an arrow object
@@ -93,8 +119,9 @@ adjacency_matrix = [[0 for _ in range(n)] for _ in range(n)]
 
 draw_arrow(adjacency_matrix, states, 0, 1)
 draw_arrow(adjacency_matrix, states, 1, 2)
+draw_arrow(adjacency_matrix, states, 2, 0)
 
-# Bind mouse events
+# Bind mouse events 
 selected_item = None
 canvas.bind("<ButtonPress-1>", on_press)
 canvas.bind("<B1-Motion>", lambda event: on_drag(event, states, adjacency_matrix))
