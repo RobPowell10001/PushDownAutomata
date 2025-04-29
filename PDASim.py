bb43a1a2a0eb942eec585ff486cf96eb2654a8bb
Chase_Fetherling <cmfetherling@crimson.ua.edu>
2025-04-28 19:23:12 -0500

    Added commenting

--- PDASim.py
+++ PDASim.py
@@ -1,4 +1,6 @@
 import json
+
+#Class which contains all the information about one specific transition in the PDA
 class Transition:
     def __init__(self, destinationState, inputSymbol, topStack, push, pop):
         self.destinationState = destinationState
@@ -10,6 +12,8 @@ class Transition:
     def toString(self):
         return f"{self.inputSymbol}, {self.topStack} {" (pop)" if self.pop else ""}" + " \N{RIGHTWARDS ARROW} " + f"{self.push}"
 
+#Class which is instantiated as states in the PDA which contain all the information about transitions which can occur from them
+#as well as their initial and final status
 class State:
     def __init__(self, name, isInitial, isFinal, transitions):
         self.name = name
@@ -17,12 +21,16 @@ class State:
         self.isFinal = isFinal
         self.transitions = transitions
 
+#Main function which does all the logical PDA work including being encoded and decoded from json
+#as well as find all current possible transitions and doing transitions the user selects
 class PDA:
     def __init__(self, states, currState):
         self.states = states
         self.stack = []
         self.currState = currState
         self.initialState = -1
+    
+    #Turns the PDA into a json data structure
     def jsonEncoding(self, fileName = "data"):
         jsonData = {}
         jsonData["states"] = []
@@ -48,6 +56,8 @@ class PDA:
         jsonData["currState"] = self.initialState
         with open(f"{fileName}.json", "w") as file:
             json.dump(jsonData, file, indent=4)
+
+    #Turns a jsonObject into a PDA 
     def jsonDecoding(self, jsonString):
         jsonObject = json.loads(jsonString) 
         self.states = []
@@ -61,12 +71,16 @@ class PDA:
             self.states.append(tempState)
             if state["isInitial"]:
                 self.currState = len(self.states) - 1
+    
+    #Helper function that returns the top of the stack
     def topStack(self):
         if len(self.stack) != 0:
             return self.stack[len(self.stack)-1]
         else:
             #print("stack is empty, returning None")
             return None
+    
+    #Function that finds all transitions of the PDA from the current state with the given input symbol
     def findTransitions(self, inputSymbol):
         acceptedTransitions = []
         #print(f"on input {inputSymbol} and topStack {self.topStack()}:")
@@ -76,6 +90,8 @@ class PDA:
                 #print("accepted!")
                 acceptedTransitions.append(transition)
         return acceptedTransitions
+    
+    #Function that does a valid transition on the PDA moving it to the correct state and doing the necessary operations on the stack
     def doTransitions(self,transition):
         if transition.pop:
             self.stack.pop()
